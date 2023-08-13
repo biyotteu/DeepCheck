81625ce404c498aca50fce6615013148e9b0f75c
reindeer002 <lsj000424@gmail.com>
2023-08-13 07:41:52 +0000

    [MOD] requirements file path

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-pip install -r ./fake_audio_detection/requirements.txt
-pip install -r ./deepfake_detection/requirements.txt
 pip install -r ./deepcheck_backend/requirements.txt
+pip install -r ./deepcheck_backend/lib/fake_audio_detection/requirements.txt
+pip install -r ./deepcheck_backend/lib/deepfake_detection/requirements.txt
