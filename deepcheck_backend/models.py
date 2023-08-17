bb5a20493ab0d56fccb68b5efca7c6c275765916
aest3ra <aest3ra@gmail.com>
2023-08-17 10:20:24 +0000

    [backend] add Survey Model

--- deepcheck_backend/models.py
+++ deepcheck_backend/models.py
@@ -11,3 +11,18 @@ class User(Base):
     # username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
+
+
+class Survey(Base):
+    __tablename__ = "survey"
+
+    userid = Column(Integer, primary_key=True, unique=True)
+    email = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    rate = Column(Integer, nullable=False)
+    satisfied = Column(String, nullable=False)
+    unsatisfied = Column(String, nullable=False)
+    unsatisfiedReason = Column(String, nullable=True)
+
+
